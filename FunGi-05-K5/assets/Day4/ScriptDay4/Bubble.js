386883b1e3d0d0ba248a63876189e9f711b5e24b
FunGi2410 <nguyennhanit2410@gmail.com>
2025-05-10 17:00:12 +0700

    add rigidbody to push bubble

--- FunGi-05-K5/assets/Day4/ScriptDay4/Bubble.js
+++ FunGi-05-K5/assets/Day4/ScriptDay4/Bubble.js
@@ -3,13 +3,14 @@ cc.Class({
     extends: cc.Component,
 
     properties: {
-        score: 1,
-
         // Label
         scoreLabel: cc.Label,
     },
 
     onLoad () {
+        this.isClicked = false;
+        this.score = 1;
+        this.rigid = this.node.getComponent(cc.RigidBody);
         // add event click
         this.node.on(cc.Node.EventType.TOUCH_END, this.onClick, this);
     },
@@ -26,12 +27,22 @@ cc.Class({
         this.scheduleOnce(() => {
             this.onMiss();
         }, timeLife);
+
+        // push up when spawn
+        let force = this._randomInt(6, 8);
+        this.pushUp(force * 100);
     },
 
     // update (dt) {},
 
+    pushUp(force){
+        if(!this.rigid) return;
+        this.rigid.linearVelocity = cc.v2(0, force); 
+    },
+
     onMiss(){
-        let event = new cc.Event.EventCustom("bubble-clicked", true);
+        if(this.isClicked) return;
+        let event = new cc.Event.EventCustom("BUBBLE_CLICK", true);
         event.detail = { score: 0 };
         this.node.dispatchEvent(event); 
 
@@ -39,12 +50,21 @@ cc.Class({
     },
 
     onClick(){
+        this.isClicked = true;
+        this.node.off(cc.Node.EventType.TOUCH_END, this.onClick, this);
+        cc.tween(this.node)
+            .to(0.5, { opacity: 0 }) // mờ dần trong 0.5 giây
+            .call(() => {
+                this.node.destroy(); // xoá node sau khi mờ
+            })
+            .start();
+
         // call spawn func
-        let event = new cc.Event.EventCustom("bubble-clicked", true);
+        let event = new cc.Event.EventCustom("BUBBLE_CLICK", true);
         event.detail = { score: this.score };
         this.node.dispatchEvent(event); 
 
-        this.node.destroy(); 
+        //this.node.destroy(); 
     },
 
     _randomInt(min, max) {
