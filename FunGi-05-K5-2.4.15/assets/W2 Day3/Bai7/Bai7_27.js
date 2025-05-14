a98e871a1460f6dd9d855d687988ed8f523290c5
FunGi <>
2025-05-14 16:10:23 +0700

    add new project in version 2.4.15, complete Bai1, Bai7

---
+++ FunGi-05-K5-2.4.15/assets/W2 Day3/Bai7/Bai7_27.js
@@ -0,0 +1,64 @@
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        stateLabel: cc.Label,
+    },
+
+    onLoad () {
+        this.stateLabel.string = "";
+    },
+
+    start () {
+        const lazyMan = new LazyMan('jack', this.stateLabel);
+        lazyMan.eat('apple').sleep(5000).eat('hamburger').sleep(3000).eat('pear');
+    },
+});
+
+class LazyMan {
+    constructor(name, label) {
+        this.queue = [];
+        this.label = label; 
+
+        this.queue.push(() => {
+            this.setStateLabel(`My name is ${name}`);
+            this.next();
+        });
+
+        setTimeout(() => this.next(), 0);
+    }
+
+    eat(food) {
+        this.queue.push(() => {
+            this.setStateLabel(`I am eating ${food}`);
+            this.next();
+        });
+        return this;
+    }
+
+    sleep(time) {
+        this.queue.push(() => {
+            this.setStateLabel('I am sleeping...');
+            setTimeout(() => {
+                this.setStateLabel(`After ${time / 1000} seconds`);
+                this.next();
+            }, time);
+        });
+        return this;
+    }
+
+    next() {
+        const fn = this.queue.shift();
+        fn && fn();
+    }
+
+    setStateLabel(message) {
+        console.log(message);
+        if (this.label) {
+            this.label.string += message + '\n';
+        }
+    }
+}
+
+
